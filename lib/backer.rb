85e4120c6a569b92e4580a108757604288556137
Taishawn King <taishawnking1@yahoo.com>
2020-01-23 01:58:16 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,8 +8,17 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project)
-    @backed_projects << project
-    project.backers << self
-  end
-end
+
+
+
+
+
+
+
+
+
+#   def back_project(project)
+#     @backed_projects << project
+#     project.backers << self
+#   end
+# end
